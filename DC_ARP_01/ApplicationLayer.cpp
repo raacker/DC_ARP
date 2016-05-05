9ce21a1124ff1d451b8851e76ca76456ca61bdd6
SeongJung0106 <fjunkimsj@naver.com>
2016-05-05 15:37:27 +0900

    applayer fix

--- DC_ARP_01/ApplicationLayer.cpp
+++ DC_ARP_01/ApplicationLayer.cpp
@@ -25,3 +25,20 @@ void CApplicationLayer::ResetHeader( )
 
 	memset( m_sHeader.app_data, 0, APP_DATA_SIZE ) ;
 }
+
+BOOL CApplicationLayer::Send(unsigned char *ppayload, int nlength)
+{
+	m_ppayload = ppayload;
+	m_length = nlength;
+
+	if(nlength <= APP_DATA_SIZE){
+		m_sHeader.app_totlen = nlength;
+		memcpy(m_sHeader.app_data,ppayload,nlength);
+		mp_UnderLayer->Send((unsigned char*) &m_sHeader, nlength+APP_HEADER_SIZE);
+	}
+}
+
+BOOL CApplicationLayer::Receive(unsigned char* pplayload)
+{
+
+}
